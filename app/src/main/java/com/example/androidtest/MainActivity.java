802a1c00cd5d2b12b112198089da4e514c914de4
gargi3099 <gargi3099@gmail.com>
2020-10-15 14:51:47 +0530

    First commit

--- app/src/main/java/com/example/androidtest/MainActivity.java
+++ app/src/main/java/com/example/androidtest/MainActivity.java
@@ -5,13 +5,14 @@ import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String[] mnamearray={"Gargi","Kriti","Jinal","Shikha","Bhavesh"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void changename(View view) {
-        Random random=new Random();
-        int r=random.nextInt(mnamearray.length-1);
-        String name=mnamearray[r];
-        //int nameresource=getResources().getIdentifier(name,"text",getApplicationContext().getPackageName());
-        Intent intent=new Intent(this,NameDisplay.class);
-        intent.putExtra("NAME",name);
+        Intent intent=new Intent(MainActivity.this,NameDisplay.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/androidtest/NameDisplay.java
+++ app/src/main/java/com/example/androidtest/NameDisplay.java
@@ -4,19 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class NameDisplay extends AppCompatActivity {
 
+    List<String> mnamearray= Arrays.asList("Gargi","Kriti","Jinal","Shikha","Bhavesh");
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name_display);
-        Intent intent=getIntent();
-        String name=intent.getStringExtra("NAME");
-        TextView textView=findViewById(R.id.textName);
+        Random random=new Random();
+        String name=mnamearray.get(random.nextInt(mnamearray.size()));
+        textView=(TextView)findViewById(R.id.textName);
         textView.setText(name);
     }
 }
